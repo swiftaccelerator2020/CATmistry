3ae304b5dc50fc960c1b47cec372e20d3a37985a
Wang Zerui <h2010155@nushigh.edu.sg>
2020-11-27 17:12:16 +0800

    added user defaults, table view controller, cell and struct. do not commit until next commit by me(zerui).

---
+++ CATmistry/learnPageTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  learnPageTableViewCell.swift
+//  CATmistry
+//
+//  Created by Wang Zerui on 27/11/20.
+//
+
+import UIKit
+
+class learnPageTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var subjectTitle: UILabel!
+    @IBOutlet weak var iconPic: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CATmistry/structsFunctions.swift
@@ -0,0 +1,16 @@
+//
+//  structsFunctions.swift
+//  CATmistry
+//
+//  Created by Wang Zerui on 27/11/20.
+//
+
+import Foundation
+
+struct topic {
+    var segueDesti: String
+    var title: String
+    var pic: String
+    var canAccess: Bool
+}
+
