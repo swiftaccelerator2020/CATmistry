bc978ef85b658b91a39b822728dbdf18e97a9d25
Wang Zerui <h2010155@nushigh.edu.sg>
2020-11-30 15:34:17 +0800

    repost

--- CATmistry/LearnSubTopicDetailsViewController.swift
+++ CATmistry/LearnSubTopicDetailsViewController.swift
@@ -11,15 +11,15 @@ class LearnSubTopicDetailsViewController: UIViewController {
     
     var specificSubTopic: SubTopic!
 
-    @IBOutlet weak var pHSlider: UISlider!
-    @IBOutlet weak var maxLabel: UILabel!
-    @IBOutlet weak var minLabel: UILabel!
-    @IBOutlet weak var definitionLabel: UILabel!
-    @IBOutlet weak var sliderValueLabel: UILabel!
-    @IBOutlet weak var subSubTopicsTableView: UITableView!
-    @IBOutlet weak var pHSlidersVertStack: UIStackView!
-    @IBOutlet var pHSliderVertStackIsVisibleConstraint: NSLayoutConstraint!
-    @IBOutlet var pHSliderVertStackIsHiddenConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pHSlider: UISlider?
+    @IBOutlet weak var maxLabel: UILabel?
+    @IBOutlet weak var minLabel: UILabel?
+    @IBOutlet weak var definitionLabel: UILabel?
+    @IBOutlet weak var sliderValueLabel: UILabel?
+    @IBOutlet weak var subSubTopicsTableView: UITableView?
+    @IBOutlet weak var pHSlidersVertStack: UIStackView?
+    @IBOutlet var pHSliderVertStackIsVisibleConstraint: NSLayoutConstraint?
+    @IBOutlet var pHSliderVertStackIsHiddenConstraint: NSLayoutConstraint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,27 +27,27 @@ class LearnSubTopicDetailsViewController: UIViewController {
         // Do any additional setup after loading the view.
         self.title = specificSubTopic.navTitle
         self.navigationController?.navigationBar.tintColor = UIColor.darkGray
-        definitionLabel.text = specificSubTopic.content.definition
+        definitionLabel?.text = specificSubTopic.content.definition
         
         if (!specificSubTopic.needSlider) {
-            pHSlidersVertStack.isHidden = true
-            pHSliderVertStackIsVisibleConstraint.isActive = false
-            pHSliderVertStackIsHiddenConstraint.isActive = true
+            pHSlidersVertStack?.isHidden = true
+            pHSliderVertStackIsVisibleConstraint?.isActive = false
+            pHSliderVertStackIsHiddenConstraint?.isActive = true
         } else {
-            pHSlidersVertStack.isHidden = false
-            pHSliderVertStackIsVisibleConstraint.isActive = true
-            pHSliderVertStackIsHiddenConstraint.isActive = false
+            pHSlidersVertStack?.isHidden = false
+            pHSliderVertStackIsVisibleConstraint?.isActive = true
+            pHSliderVertStackIsHiddenConstraint?.isActive = false
         }
         
         if (specificSubTopic.subTopics == nil) {
-            subSubTopicsTableView.isHidden = true
+            subSubTopicsTableView?.isHidden = true
         }
         
     }
     
 
     @IBAction func sliderChangedValue(_ sender: UISlider) {
-        sliderValueLabel.text = "Current pH: " + String(Int(sender.value.rounded()))
+        sliderValueLabel?.text = "Current pH: " + String(Int(sender.value.rounded()))
     }
     /*
     // MARK: - Navigation
@@ -58,5 +58,23 @@ class LearnSubTopicDetailsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    func numberOfSections(in tableView: UITableView) -> Int {
+            // #warning Incomplete implementation, return the number of sections
+            return 1
+        }
 
+        func tableView( tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // #warning Incomplete implementation, return the number of rows
+            return specificSubTopic.subTopics!.count
+        }
+
+        func tableView( tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            80.0
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "subSubTopicCell", for: indexPath)
+            
+            return cell
+        }
 }
