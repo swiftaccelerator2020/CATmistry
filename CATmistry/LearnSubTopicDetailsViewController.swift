0f42ec1ff0023c7697ca7875f55faeadf56298f5
neohaojun <neohaojun@gmail.com>
2020-11-30 10:06:34 +0800

    refactored some iboutlets for greater clarity, added a new vc for subsubtopics, and adjusted some if else statements that were not working as expected

--- CATmistry/LearnSubTopicDetailsViewController.swift
+++ CATmistry/LearnSubTopicDetailsViewController.swift
@@ -11,35 +11,45 @@ class LearnSubTopicDetailsViewController: UIViewController {
     
     var specificSubTopic: SubTopic!
 
-    @IBOutlet weak var PHSlider: UISlider!
+    @IBOutlet weak var pHSlider: UISlider!
     @IBOutlet weak var maxLabel: UILabel!
     @IBOutlet weak var minLabel: UILabel!
     @IBOutlet weak var definitionLabel: UILabel!
-    @IBOutlet weak var sliderValue: UILabel!
-    @IBOutlet weak var contentTable: UITableView!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var subSubTopicsTableView: UITableView!
+    @IBOutlet weak var pHSliderVertStack: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        self.title = specificSubTopic.title
+        self.title = specificSubTopic.navTitle
         self.navigationController?.navigationBar.tintColor = UIColor.darkGray
         definitionLabel.text = specificSubTopic.content.definition
-        if ((specificSubTopic.subTopics?.isEmpty) != nil){
-            contentTable.isHidden = true
-        }
         
-        if (!specificSubTopic.needSlider){
-            sliderValue.isHidden = true
-            PHSlider.isHidden = true
+        if (!specificSubTopic.needSlider) {
+            pHSliderVertStack.isHidden = true
+            sliderValueLabel.isHidden = true
+            pHSlider.isHidden = true
             maxLabel.isHidden = true
             minLabel.isHidden = true
+        } else {
+            pHSliderVertStack.isHidden = false
+            sliderValueLabel.isHidden = false
+            pHSlider.isHidden = false
+            maxLabel.isHidden = false
+            minLabel.isHidden = false
         }
+        
+        if (specificSubTopic.subTopics == nil) {
+            subSubTopicsTableView.isHidden = true
+        }
+        
     }
     
 
     @IBAction func sliderChangedValue(_ sender: UISlider) {
-        sliderValue.text = String(Int(sender.value.rounded()))
+        sliderValueLabel.text = "Current pH: " + String(Int(sender.value.rounded()))
     }
     /*
     // MARK: - Navigation
--- CATmistry/structsFunctions.swift
+++ CATmistry/structsFunctions.swift
@@ -29,6 +29,7 @@ struct SubTopic {
     var needSlider: Bool
     var content: SubContent
     var subTopics: Array<SubSubTopic>?
+    var navTitle: String
 }
 
 struct SubSubTopic {
@@ -66,27 +67,33 @@ struct QuizQuestion {
 var learnTopics = [
     Topic(title: "Gas Tests", pic: "gas-tests.png", pointsNeeded: 0, subTopics: [
         SubTopic(title: "What are Gas Tests?", pic: "gas-tests.png", needSlider: false, content:
-                    SubContent(definition: ""),
-                 subTopics: nil),
-        SubTopic(title: "Take the Quiz", pic: "quiz.png", needSlider: false, content: SubContent(definition: ""), subTopics: [])
+                    SubContent(definition: "some filler text for show"),
+                 subTopics: [
+                    SubSubTopic(topic: "Test for H₂", picture: "h2.png", id: 1, content: SubSubContent(image: "pic.png")),
+                    SubSubTopic(topic: "Test for CO₂", picture: "co2.png", id: 2, content: SubSubContent(image: "pic.png")),
+                    SubSubTopic(topic: "Test for O₂", picture: "o2.png", id: 3, content: SubSubContent(image: "pic.png")),
+                    SubSubTopic(topic: "Test for NH₃", picture: "nh3.png", id: 4, content: SubSubContent(image: "pic.png"))
+                 ], navTitle: "Gas Tests"),
+        SubTopic(title: "Take the Quiz", pic: "quiz.png", needSlider: false, content: SubContent(definition: ""), subTopics: [], navTitle: "Take the Quiz")
     ], errorLabel: ""),
     
     Topic(title: "Separation Methods", pic: "separation-methods.png", pointsNeeded: 10, subTopics: [
-        SubTopic(title: "What is Separation?", pic: "separation-methods.png", needSlider: false, content: SubContent(definition: "A separation process is a method that converts a mixture or solution of chemical substances into two or more distinct product mixtures. \n\nSeparations exploit differences in chemical properties or physical properties (such as size, shape, mass, density, or chemical affinity) between the constituents of a mixture."), subTopics: []),
-        SubTopic(title: "Take the Quiz", pic: "quiz.png", needSlider: false, content: SubContent(definition: ""), subTopics: [])
+        SubTopic(title: "What is Separation?", pic: "separation-methods.png", needSlider: false, content: SubContent(definition: "A separation process is a method that converts a mixture or solution of chemical substances into two or more distinct product mixtures. \n\nSeparations exploit differences in chemical properties or physical properties (such as size, shape, mass, density, or chemical affinity) between the constituents of a mixture."), subTopics: [], navTitle: "Separation"),
+        SubTopic(title: "Take the Quiz", pic: "quiz.png", needSlider: false, content: SubContent(definition: ""), subTopics: [], navTitle: "Take the Quiz")
     ], errorLabel: "Please complete Chapter 1"),
     
     Topic(title: "pH and Indicators", pic: "pH-and-indicators.png", pointsNeeded: 20, subTopics: [
-        SubTopic(title: "What is pH?", pic: "pH-and-indicators.png", needSlider: false, content: SubContent(definition: "pH (the Power of Hydrogen) is a scale of 1 to 14 used to specify the acidity or basicity of a solution. The pH scale indicates the concentration of hydrogen ions in a solution.\n\nA lower pH (1 - 6.9) indicates that the solution is acidic, while a higher pH (7.1 - 14) indicates that the solution is alkaline. A pH of 7 indicates that the solution is neutral (neither acidic nor alkaline)."), subTopics: []),
-        SubTopic(title: "What are Indicators?", pic: "pH-and-indicators.png", needSlider: false, content: SubContent(definition: "Indicators are chemicals which turn into different colours depending on the pH of the solution. The transition range is the range in which the indicator changes from one colour to the other."), subTopics: []),
-        SubTopic(title: "Take the Quiz", pic: "quiz.png", needSlider: false, content: SubContent(definition: ""), subTopics: [])
+        SubTopic(title: "What is pH?", pic: "pH-and-indicators.png", needSlider: false, content: SubContent(definition: "pH (the Power of Hydrogen) is a scale of 1 to 14 used to specify the acidity or basicity of a solution. The pH scale indicates the concentration of hydrogen ions in a solution.\n\nA lower pH (1 - 6.9) indicates that the solution is acidic, while a higher pH (7.1 - 14) indicates that the solution is alkaline. A pH of 7 indicates that the solution is neutral (neither acidic nor alkaline)."), subTopics: [], navTitle: "pH"),
+        SubTopic(title: "What are Indicators?", pic: "pH-and-indicators.png", needSlider: false, content: SubContent(definition: "Indicators are chemicals which turn into different colours depending on the pH of the solution. The transition range is the range in which the indicator changes from one colour to the other."), subTopics: [], navTitle: "Indicators"),
+        SubTopic(title: "Take the Quiz", pic: "quiz.png", needSlider: false, content: SubContent(definition: ""), subTopics: [], navTitle: "Take the Quiz")
     ], errorLabel: "Please complete Chapter 2"),
     
     Topic(title: "Periodic Table", pic: "periodic-table.png", pointsNeeded: 30, subTopics: [
-        SubTopic(title: "What is the Periodic Table?", pic: "periodic-table.png", needSlider: false, content: SubContent(definition: "A periodic table is a chart that shows chemical elements in order of their atomic number. \n\nElements with similar properties are arranged in the same column (group), and elements with the same number of electron shells are arranged in the same row (period)."), subTopics: []),
-        SubTopic(title: "What are Elements?", pic: "periodic-table.png", needSlider: false, content: SubContent(definition: ""), subTopics: []),
-        SubTopic(title: "First 18 Elements", pic: "periodic-table.png", needSlider: false, content: SubContent(definition: ""), subTopics: []),
-        SubTopic(title: "Take the Quiz", pic: "quiz.png", needSlider: false, content: SubContent(definition: ""), subTopics: [])
+        SubTopic(title: "What is the Periodic Table?", pic: "periodic-table.png", needSlider: false, content: SubContent(definition: "A periodic table is a chart that shows chemical elements in order of their atomic number. \n\nElements with similar properties are arranged in the same column (group), and elements with the same number of electron shells are arranged in the same row (period)."), subTopics: [], navTitle: "Periodic Table"),
+        SubTopic(title: "What are Elements?", pic: "periodic-table.png", needSlider: false, content: SubContent(definition: ""), subTopics: [], navTitle: "Elements"),
+        SubTopic(title: "First 18 Elements", pic: "periodic-table.png", needSlider: false, content: SubContent(definition: ""), subTopics: [], navTitle: "First 18 Elements"),
+        SubTopic(title: "Groups' Properties", pic: "periodic-table.png", needSlider: false, content: SubContent(definition: ""), subTopics: [], navTitle: "Groups' Properties"),
+        SubTopic(title: "Take the Quiz", pic: "quiz.png", needSlider: false, content: SubContent(definition: ""), subTopics: [], navTitle: "Take the Quiz")
     ], errorLabel: "Please complete Chapter 3"),
 ]
 
