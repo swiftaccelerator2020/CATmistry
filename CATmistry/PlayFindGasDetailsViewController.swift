60a04ae0fa935d7acb43a6a0cf877c3ff8f9aad2
neohaojun <neohaojun@gmail.com>
2020-12-02 22:13:18 +0800

    added PlayFindGasDetailsViewController, PlayRetrieveSolidsDetailsViewController PlayAdjustPhDetailsViewController

---
+++ CATmistry/PlayFindGasDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlayFindGasDetailsViewController.swift
+//  CATmistry
+//
+//  Created by N HJ on 2/12/20.
+//
+
+import UIKit
+
+class PlayFindGasDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
