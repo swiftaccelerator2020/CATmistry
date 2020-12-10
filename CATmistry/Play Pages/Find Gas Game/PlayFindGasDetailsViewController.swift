529b62e772fd029c0eb1fd702944c3e282e80892
neohaojun <neohaojun@gmail.com>
2020-12-10 11:13:36 +0800

    tried out some new panning stuff, made some stuff user interaction enabled

--- CATmistry/Play Pages/Find Gas Game/PlayFindGasDetailsViewController.swift
+++ CATmistry/Play Pages/Find Gas Game/PlayFindGasDetailsViewController.swift
@@ -11,6 +11,7 @@ import SwiftConfettiView
 class PlayFindGasDetailsViewController: UIViewController {
     
     var currentLevel = 0
+    var initialCenter = CGPoint()  // The initial center point of the view.
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,23 +19,45 @@ class PlayFindGasDetailsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func handlePan(_ gesture: UIPanGestureRecognizer) {
-      // 1
-      let translation = gesture.translation(in: view)
-
-      // 2
-      guard let gestureView = gesture.view else {
-        return
-      }
-
-      gestureView.center = CGPoint(
-        x: gestureView.center.x + translation.x,
-        y: gestureView.center.y + translation.y
-      )
-
-      // 3
-      gesture.setTranslation(.zero, in: view)
-    }    
+//    @IBAction func handlePan(_ gesture: UIPanGestureRecognizer) {
+//      // 1
+//      let translation = gesture.translation(in: view)
+//
+//      // 2
+//      guard let gestureView = gesture.view else {
+//        return
+//      }
+//
+//      gestureView.center = CGPoint(
+//        x: gestureView.center.x + translation.x,
+//        y: gestureView.center.y + translation.y
+//      )
+//
+//      // 3
+//      gesture.setTranslation(.zero, in: view)
+//    }
+    
+    @IBAction func panPiece(_ gestureRecognizer : UIPanGestureRecognizer) {
+       guard gestureRecognizer.view != nil else {return}
+       let piece = gestureRecognizer.view!
+       // Get the changes in the X and Y directions relative to
+       // the superview's coordinate space.
+       let translation = gestureRecognizer.translation(in: piece.superview)
+       if gestureRecognizer.state == .began {
+          // Save the view's original position.
+          self.initialCenter = piece.center
+       }
+          // Update the position for the .began, .changed, and .ended states
+       if gestureRecognizer.state != .cancelled {
+          // Add the X and Y translation to the view's original position.
+          let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+          piece.center = newCenter
+       }
+       else {
+          // On cancellation, return the piece to its original location.
+          piece.center = initialCenter
+       }
+    }
 
     /*
     // MARK: - Navigation
