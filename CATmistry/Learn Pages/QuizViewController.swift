142a6ca0ff2531e3ec1476792e31131c487a5fff
Wang Zerui <h2010155@nushigh.edu.sg>
2020-12-13 19:12:55 +0800

    crash fix attempt V

--- CATmistry/Learn Pages/QuizViewController.swift
+++ CATmistry/Learn Pages/QuizViewController.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 class QuizViewController: UIViewController {
-
+    
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var optionOne: UIButton!
     @IBOutlet weak var optionTwo: UIButton!
     @IBOutlet weak var optionThree: UIButton!
     @IBOutlet weak var optionFour: UIButton!
     @IBOutlet weak var timeLeft: UIProgressView!
-    
     var progressBarTimer: Timer!
+    
     var index = 0
     var question = [QuizQuestion(question: "Hi there are you bob", options: ["hi", "yes", "aa", "no u"], correctAnswer: 1), QuizQuestion(question: "Hi theafsfasfsare are you bob", options: ["hi", "yes", "aa", "no u"], correctAnswer: 1)]
     
@@ -37,10 +37,10 @@ class QuizViewController: UIViewController {
         questionLabel.text = question[index].question
         
         self.progressBarTimer = Timer.scheduledTimer(timeInterval: 0.025, target: self, selector: #selector(QuizViewController.updateProgressView), userInfo: nil, repeats: true)
-
+        
         timeLeft.transform = timeLeft.transform.scaledBy(x: 1, y: 2)
         
-
+        
         
         firstLoad = false
         // Do any additional setup after loading the view.
@@ -48,32 +48,33 @@ class QuizViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         timeLeft.progress = 0
-    
+        
         if (index >= question.count-1){
             stopTimer()
             performSegue(withIdentifier: "quizEnded", sender: nil)
+            progressBarTimer = nil
         }
         
         if !firstLoad{
             optionOne.titleLabel!.text = question[index].options[0]
-        
+            
             optionTwo.titleLabel!.text = question[index].options[1]
-        
+            
             optionThree.titleLabel!.text = question[index].options[2]
-        
+            
             optionFour.titleLabel!.text = question[index].options[3]
-        
+            
             questionLabel.text = question[index].question
-        
+            
             self.progressBarTimer = Timer.scheduledTimer(timeInterval: 0.025, target: self, selector: #selector(QuizViewController.updateProgressView), userInfo: nil, repeats: true)
         }
     }
-
+    
     @IBAction func optionOneClicked(_ sender: Any) {
         stopTimer()
-        if (question[index].correctAnswer == 1){
+        if (question[index].correctAnswer == 1) {
             performSegue(withIdentifier: "correctAns", sender: nil)
-        }else{
+        } else {
             performSegue(withIdentifier: "wrongAns", sender: nil)
         }
         index += 1
@@ -81,9 +82,9 @@ class QuizViewController: UIViewController {
     
     @IBAction func optionTwoClicked(_ sender: Any) {
         stopTimer()
-        if (question[index].correctAnswer == 2){
+        if (question[index].correctAnswer == 2) {
             performSegue(withIdentifier: "correctAns", sender: nil)
-        }else{
+        } else {
             performSegue(withIdentifier: "wrongAns", sender: nil)
         }
         index += 1
@@ -91,9 +92,9 @@ class QuizViewController: UIViewController {
     
     @IBAction func optionThreeClicked(_ sender: Any) {
         stopTimer()
-        if (question[index].correctAnswer == 3){
+        if (question[index].correctAnswer == 3) {
             performSegue(withIdentifier: "correctAns", sender: nil)
-        }else{
+        } else {
             performSegue(withIdentifier: "wrongAns", sender: nil)
         }
         index += 1
@@ -101,36 +102,33 @@ class QuizViewController: UIViewController {
     
     @IBAction func optionFourClicked(_ sender: Any) {
         stopTimer()
-        if (question[index].correctAnswer == 4){
+        if (question[index].correctAnswer == 4) {
             performSegue(withIdentifier: "correctAns", sender: nil)
-        }else{
+        } else {
             performSegue(withIdentifier: "wrongAns", sender: nil)
         }
         index += 1
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     @objc func updateProgressView(){
         timeLeft.progress += 0.005
-        if(timeLeft.progress == 1.0)
-        {
+        if (timeLeft.progress == 1.0) {
             stopTimer()
-            timeLeft.progress = 0
             performSegue(withIdentifier: "wrongAns", sender: nil)
             index += 1
         }
     }
     
-    func stopTimer(){
+    func stopTimer() {
         progressBarTimer.invalidate()
-        progressBarTimer = nil
         timeLeft.progress = 0
     }
 }
