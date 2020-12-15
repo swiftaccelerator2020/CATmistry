2f4793fb7bc0450cd8fd1874419b9c4d607b8683
neohaojun <neohaojun@gmail.com>
2020-12-15 13:06:59 +0800

    solved go back to learning button not working

--- CATmistry/Learn Pages/QuizCorrectAnswerViewController.swift
+++ CATmistry/Learn Pages/QuizCorrectAnswerViewController.swift
@@ -11,14 +11,14 @@ import UIKit
 class QuizCorrectAnswerViewController: UIViewController {
     
     var currentQuestion: Int!
-
+    
     @IBOutlet var nextQuestionButton: UIButton!
     @IBOutlet var congratsLabel: UILabel!
     @IBOutlet var goBackLearningButton: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         nextQuestionButton.layer.cornerRadius = 25
         
@@ -33,7 +33,7 @@ class QuizCorrectAnswerViewController: UIViewController {
         confettiView.isUserInteractionEnabled = false
         confettiView.startConfetti()
     }
-
+    
     @IBAction func nextGameClicked(_: Any) {
         if 9 - currentQuestion == 0 {
             presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
@@ -41,19 +41,21 @@ class QuizCorrectAnswerViewController: UIViewController {
             dismiss(animated: true, completion: nil)
         }
     }
-
+    
     @IBAction func restartClicked(_: Any) {
         let alert = UIAlertController(title: "Are you sure you would like to exit?", message: "All progress wil be lost, and you will be taken back to the learn page.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Continue", style: UIAlertAction.Style.default, handler: { _ in
-            self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+            self.navigationController?.setNavigationBarHidden(false, animated: false)
+            self.tabBarController?.tabBar.isHidden = false
+            self.navigationController!.popToRootViewController(animated: true)
         }))
         alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
         present(alert, animated: true, completion: nil)
     }
-
+    
     /*
      // MARK: - Navigation
-
+     
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destination.
--- CATmistry/Learn Pages/QuizWrongAnswerViewController.swift
+++ CATmistry/Learn Pages/QuizWrongAnswerViewController.swift
@@ -8,16 +8,16 @@
 import UIKit
 
 class QuizWrongAnswerViewController: UIViewController {
-
+    
     var currentQuestion: Int!
-
+    
     @IBOutlet var nextQuestionButton: UIButton!
     @IBOutlet var goBackLearningButton: UIButton!
     @IBOutlet var sadLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         nextQuestionButton.layer.cornerRadius = 25
         
@@ -27,7 +27,7 @@ class QuizWrongAnswerViewController: UIViewController {
             sadLabel.text = "Oh no! \n\nYour answer was wrong, but don't worry! You have \(String(9 - currentQuestion)) questions left in this quiz. Try harder next time!"
         }
     }
-
+    
     @IBAction func nextGameClicked(_: Any) {
         if 9 - currentQuestion == 0 {
             presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
@@ -35,24 +35,26 @@ class QuizWrongAnswerViewController: UIViewController {
             dismiss(animated: true, completion: nil)
         }
     }
-
+    
     @IBAction func restartClicked(_: Any) {
         let alert = UIAlertController(title: "Are you sure you would like to exit?", message: "All progress wil be lost, and you will be taken back to the learn page.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Continue", style: UIAlertAction.Style.default, handler: { _ in
-            self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+            self.navigationController?.setNavigationBarHidden(false, animated: false)
+            self.tabBarController?.tabBar.isHidden = false
+            self.navigationController!.popToRootViewController(animated: true)
         }))
         alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
         present(alert, animated: true, completion: nil)
     }
-
+    
     /*
      //MARK: - Navigation
-
+     
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destination.
      // Pass the selected object to the new view controller.
      }
      */
-
+    
 }
