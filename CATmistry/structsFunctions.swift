45c36334cc975320a3bda665a524be46c11dbe07
Wang Zerui <h2010155@nushigh.edu.sg>
2020-12-15 20:28:08 +0800

    added font

--- CATmistry/structsFunctions.swift
+++ CATmistry/structsFunctions.swift
@@ -850,3 +850,81 @@ func changePoints(_ valueChanged: Int){
     let ud = UserDefaults.standard
     ud.set(points, forKey: "points")
 }
+
+// MARK: - Fonts
+
+import UIKit
+
+struct AppFontName {
+    static let regular = "OpenDyslexic-Regular"
+    static let bold = "OpenDyslexic-Bold"
+    static let italic = "OpenDyslexic-Italic"
+}
+
+extension UIFontDescriptor.AttributeName {
+    static let nsctFontUIUsage = UIFontDescriptor.AttributeName(rawValue: "NSCTFontUIUsageAttribute")
+}
+
+extension UIFont {
+    static var isOverrided: Bool = false
+
+    @objc class func mySystemFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.regular, size: size)!
+    }
+
+    @objc class func myBoldSystemFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.bold, size: size)!
+    }
+
+    @objc class func myItalicSystemFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.italic, size: size)!
+    }
+
+    @objc convenience init(myCoder aDecoder: NSCoder) {
+        guard
+            let fontDescriptor = aDecoder.decodeObject(forKey: "UIFontDescriptor") as? UIFontDescriptor,
+            let fontAttribute = fontDescriptor.fontAttributes[.nsctFontUIUsage] as? String else {
+                self.init(myCoder: aDecoder)
+                return
+        }
+        var fontName = ""
+        switch fontAttribute {
+        case "CTFontRegularUsage":
+            fontName = AppFontName.regular
+        case "CTFontEmphasizedUsage", "CTFontBoldUsage":
+            fontName = AppFontName.bold
+        case "CTFontObliqueUsage":
+            fontName = AppFontName.italic
+        default:
+            fontName = AppFontName.regular
+        }
+        self.init(name: fontName, size: fontDescriptor.pointSize)!
+    }
+
+    class func overrideInitialize() {
+        guard self == UIFont.self, !isOverrided else { return }
+
+        // Avoid method swizzling run twice and revert to original initialize function
+        isOverrided = true
+
+        if let systemFontMethod = class_getClassMethod(self, #selector(systemFont(ofSize:))),
+            let mySystemFontMethod = class_getClassMethod(self, #selector(mySystemFont(ofSize:))) {
+            method_exchangeImplementations(systemFontMethod, mySystemFontMethod)
+        }
+
+        if let boldSystemFontMethod = class_getClassMethod(self, #selector(boldSystemFont(ofSize:))),
+            let myBoldSystemFontMethod = class_getClassMethod(self, #selector(myBoldSystemFont(ofSize:))) {
+            method_exchangeImplementations(boldSystemFontMethod, myBoldSystemFontMethod)
+        }
+
+        if let italicSystemFontMethod = class_getClassMethod(self, #selector(italicSystemFont(ofSize:))),
+            let myItalicSystemFontMethod = class_getClassMethod(self, #selector(myItalicSystemFont(ofSize:))) {
+            method_exchangeImplementations(italicSystemFontMethod, myItalicSystemFontMethod)
+        }
+
+        if let initCoderMethod = class_getInstanceMethod(self, #selector(UIFontDescriptor.init(coder:))), // Trick to get over the lack of UIFont.init(coder:))
+            let myInitCoderMethod = class_getInstanceMethod(self, #selector(UIFont.init(myCoder:))) {
+            method_exchangeImplementations(initCoderMethod, myInitCoderMethod)
+        }
+    }
+}
